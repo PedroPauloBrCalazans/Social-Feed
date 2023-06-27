5e318ddfa7301e7bc712bbbd4e46d26eeaa15d14
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-06-27 18:05:43 -0300

    chore: Configurações principais do projeto + CSS Module.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <h1>post</h1>
+    </div>
+  );
+}
+
+export default App;
