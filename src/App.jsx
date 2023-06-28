3e78dc21fa14fb1492b42f25cbac464fefea787d
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-06-28 14:50:32 -0300

    feat: Iniciando component Sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
@@ -6,7 +9,12 @@ function App() {
     <div>
       <Header />
 
-      <h1>post</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h1>post</h1>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        src="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fycm98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=50"
+        alt=""
+        className={styles.cover}
+      />
+
+      <div className={styles.profile}>
+        <strong>Pedro Paulo</strong>
+        <span>Engenheiro automotivo</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
